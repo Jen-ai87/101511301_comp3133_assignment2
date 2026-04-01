84a61e03c2c34c52a7efda9f31520ca0c4acce8b
Jen Henry <jokodzena@gmail.com>
2026-03-31 22:33:30 -0400

    Fix: add /graphql suffix to production API URL

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://101511301-comp3133-assignment2-lo67.vercel.app/',
+  apiUrl: 'https://101511301-comp3133-assignment2-back.vercel.app/graphql',
 };
